e3f8940b5b6e67e406237f3f517ba642f2f6a49c
Michael Merz <dermicha@cameo.io>
2014-06-23 23:16:50 +0200

    fixed api call to current spek

--- app/controllers/API/Shows.scala
+++ app/controllers/API/Shows.scala
@@ -51,16 +51,17 @@ object Shows extends Controller {
         Ok(Json.obj(
           "status" -> "OK",
           "stationId" -> showApiCall.stationId,
-          "stationName" -> "KW-tv",
+          "stationName" -> showApiCall.stationId,
           "stationLogoUrl" -> "http://www.wiwo-wildau.de/images/neue-mitte-2010/gewerbe-logos/kw-tv.jpg",
+          "stationLogoShow" -> "TRUE",
           "stationMainColor" -> "#112244",
           "channelId" -> showApiCall.channelId,
-          "channelName" -> "KW-tv SAT",
-          "showTitle" -> "Lokal aktuell",
+          "channelName" -> showApiCall.channelId + " SAT",
+          "showTitle" -> "Lokal super aktuell",
           "showSubtitle" -> "Ausgabe vom 23.05.2014",
           "showLogoUrl" -> "http://images.telvi.de/images/originals/1543d38645b86053c379d529.jpg",
-          "showVideoHDUrl" -> "http://player.vimeo.com/external/97516020.hd.mp4?s=d91cf2ee8f54883c8d9faec97c4f550b",
-          "showVideoSDUrl" -> "http://player.vimeo.com/external/97516020.sd.mp4?s=103e0293ad0ea739a710bd3e232d6411",
+          "showVideoHDUrl" -> "http://cdn.mabb.indarium.de/testContent/big_buck_bunny_720p_h264.mov",
+          "showVideoSDUrl" -> "http://cdn.mabb.indarium.de/testContent/big_buck_bunny_480p_h264.mov",
           "showEndInfo" -> "<h2>Sendetermine</h2><table><tr><td>Dienstag</td><td>18:00</td></tr><tr><td>Donnerstag</td><td>15:00</td></tr><tr><td>Sonntag</td><td>19:00</td></tr></table>",
           "rootPortalURL" -> "http://hbbtv.daserste.de/"
         )
