97fdbd3fdbef683b69e80544d377fdb9747cc456
Michael Merz <dermicha@cameo.io>
2014-09-09 14:42:09 +0200

    fixed home dir

--- start_with_secrets.sh
+++ start_with_secrets.sh
@@ -3,8 +3,8 @@
 # processname: hbbtvplugin
 
 # User running the Play process
-USER=mywork
-USER_HOME=/home/mywork
+USER=play
+USER_HOME=/opt/playApps/hbbTVPlugin
 
 # Java home, add java and play to path
 # export JAVA_HOME=$USER_HOME/java_home
@@ -20,7 +20,8 @@ case "$1" in
   start)
     echo -n "Starting Play service"
     rm -f ${APP_PATH}/RUNNING_PID
-    su $USER -c "$APP_PATH/bin/hbbtvplugin $APP_OPTS >/dev/null" &
+    #su $USER -c "$APP_PATH/bin/hbbtvplugin $APP_OPTS >/dev/null" &
+    $APP_PATH/bin/hbbtvplugin $APP_OPTS >/dev/null &
     RETVAL=$?
     ;;
   stop)
