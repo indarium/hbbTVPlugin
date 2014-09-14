aadac0688d92d8184f654dc196282942a567e3cc
Michael Merz <dermicha@cameo.io>
2014-09-14 22:59:57 +0200

    fixed config / spelling

--- app/actors/ShowCrawler.scala
+++ app/actors/ShowCrawler.scala
@@ -56,7 +56,7 @@ class ShowCrawler extends Actor {
         case Some(show) =>
           Show.findShowById(show.ID).map {
             case Some(existingShow) =>
-              log.info("noting to do for: " + show.toString)
+              log.info("nothing to do for: " + show.toString)
               context.system.scheduler.scheduleOnce(
                 Duration.create(crawlerPeriod, TimeUnit.MINUTES),
                 self,
