22395208a9a16b13a57a7c0ed40edcfbc410885d
Michael Merz <dermicha@cameo.io>
2015-08-24 13:56:26 +0200

    try to fix exception handling

--- app/actors/VideoDownloadActor.scala
+++ app/actors/VideoDownloadActor.scala
@@ -22,35 +22,36 @@ class VideoDownloadActor extends Actor {
   val minFileSize = Play.configuration.getLong("hms.minFileSize").get
 
   def receive = {
-    case meta: ShowMetaData => try {
-      val target = File.createTempFile(meta.stationId, meta.channelId)
+    case meta: ShowMetaData =>
+      try {
+        val target = File.createTempFile(meta.stationId, meta.channelId)
 
-      log.debug("target file: " + target.getPath + " " + target.getName)
+        log.debug("target file: " + target.getPath + " " + target.getName)
 
-      val source = meta.sourceVideoUrl match {
-        case Some(u) => u
-        case None => throw new FileNotFoundException("missing download URL")
+        val source = meta.sourceVideoUrl match {
+          case Some(u) => u
+          case None => throw new FileNotFoundException("missing download URL")
+        }
+
+        //val os = new FileOutputStream(target)
+        //Downloader.downloadFile(source, os)
+        val f = AuthDownloader.downloadFile(source.toString, target)
+        f.onSuccess {
+          case Some(downloadedFile: File) =>
+            if (downloadedFile.length > minFileSize) {
+              meta.localVideoFile = Some(target)
+              sender() ! VideoDownloadSuccess(meta)
+            }
+            else {
+              log.error(s"downloaded file size limit: ${minFileSize}")
+              log.error(s"downloaded file to small: ${downloadedFile.length}")
+              sender() ! VideoDownloadFailure(meta, new Exception("downloaded file to small: %d".format(target.length)))
+            }
+        }
+      } catch {
+        case t: Throwable =>
+          log.error("downloading '%s' failed: %s".format(meta.sourceVideoUrl, t.getMessage))
+          sender() ! VideoDownloadFailure(meta, t)
       }
-
-      //val os = new FileOutputStream(target)
-      //Downloader.downloadFile(source, os)
-      val f = AuthDownloader.downloadFile(source.toString, target)
-      f.onSuccess {
-        case Some(downloadedFile: File) =>
-          if (downloadedFile.length > minFileSize) {
-            meta.localVideoFile = Some(target)
-            sender() ! VideoDownloadSuccess(meta)
-          }
-          else {
-            log.error(s"downloaded file size limit: ${minFileSize}")
-            log.error(s"downloaded file to small: ${downloadedFile.length}")
-            sender() ! VideoDownloadFailure(meta, new Exception("downloaded file to small: %d".format(target.length)))
-          }
-      }
-    } catch {
-      case e: Exception =>
-        log.error("downloading '%s' failed: %s".format(meta.sourceVideoUrl, e.getMessage))
-        sender() ! VideoDownloadFailure(meta, e)
-    }
   }
 }
