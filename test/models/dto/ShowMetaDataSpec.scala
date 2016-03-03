01b112be8e73351e255fc341a880e23da1d3b8da
cvandrei <dawning_sun@mailbox.org>
2016-03-03 17:58:59 +0100

    improve test coverage: convert ShowMetaData to JSON

--- test/models/dto/ShowMetaDataSpec.scala
+++ test/models/dto/ShowMetaDataSpec.scala
@@ -17,7 +17,7 @@ class ShowMetaDataSpec extends Specification with PlayRunners {
 
   "json library" should {
 
-    "convert object to json" in {
+    "convert object (all fields set) to json" in {
       running(FakeApplication()) {
 
         // prepare
@@ -57,6 +57,65 @@ class ShowMetaDataSpec extends Specification with PlayRunners {
         (json \ "vimeoDone").asOpt[Boolean] mustEqual meta.vimeoDone
         (json \ "vimeoId").asOpt[Long] mustEqual meta.vimeoId
         (json \ "vimeoEncodingStatus").asOpt[VimeoEncodingStatus] mustEqual meta.vimeoEncodingStatus
+
+      }
+    }
+
+    "convert object (only mandatory fields set) to json" in {
+      running(FakeApplication()) {
+
+        // prepare
+        val meta = ShowMetaDataHelper.defaultObject("SAT", "MV1", -123L)
+        meta.hmsStationId = None
+        meta.stationName = None
+        meta.stationMainColor = None
+        meta.channelName = None
+        meta.showTitle = None
+        meta.showId = None
+        meta.showSubtitle = None
+        meta.showSourceTitle = None
+        meta.showEndInfo = None
+        meta.sourceFilename = None
+        meta.vimeo = None
+        meta.currentAccessToken = None
+        meta.vimeoDone = None
+        meta.vimeoId = None
+        meta.vimeoEncodingStatus = None
+
+        // test
+        val json = Json.toJson(meta)
+
+        // verify
+        (json \ "stationId").as[String] mustEqual meta.stationId
+        (json \ "channelId").as[String] mustEqual meta.channelId
+        (json \ "hmsStationId").asOpt[String] mustEqual None
+        (json \ "stationName").asOpt[String] mustEqual None
+        ShowMetaData.parseOptUrl(json, "stationLogoUrl") mustEqual meta.stationLogoUrl
+        (json \ "stationLogoShow").as[Boolean] mustEqual meta.stationLogoShow
+        (json \ "stationMainColor").asOpt[String] mustEqual None
+
+        (json \ "channelName").asOpt[String] mustEqual None
+        (json \ "showTitle").asOpt[String] mustEqual None
+        (json \ "showId").asOpt[Long] mustEqual None
+        (json \ "showSubtitle").asOpt[String] mustEqual None
+        (json \ "showSourceTitle").asOpt[String] mustEqual None
+        ShowMetaData.parseOptUrl(json, "showLogoUrl") mustEqual meta.showLogoUrl
+        (json \ "showLength").as[Long] mustEqual meta.showLength
+        (json \ "showEndInfo").asOpt[String] mustEqual None
+        ShowMetaData.parseOptUrl(json, "rootPortalUrl") mustEqual meta.rootPortalUrl
+
+        (json \ "isHD").as[Boolean] mustEqual meta.isHD
+        (json \ "sourceFilename").asOpt[String] mustEqual None
+        ShowMetaData.parseOptUrl(json, "sourceVideoUrl") mustEqual meta.sourceVideoUrl
+        ShowMetaData.parseOptFile(json, "localVideoFile").get.getAbsolutePath mustEqual meta.localVideoFile.get.getAbsolutePath
+        ShowMetaData.parseOptUrl(json, "publicVideoUrl") mustEqual meta.publicVideoUrl
+
+        (json \ "currentAccessToken").asOpt[String] mustEqual None
+
+        (json \ "vimeo").asOpt[Boolean] mustEqual None
+        (json \ "vimeoDone").asOpt[Boolean] mustEqual None
+        (json \ "vimeoId").asOpt[Long] mustEqual None
+        (json \ "vimeoEncodingStatus").asOpt[VimeoEncodingStatus] mustEqual None
 
       }
     }
