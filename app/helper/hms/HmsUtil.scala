307c4a6e418b8bc2c707c464cb2222393c338a1b
cvandrei <crille@indarium.de>
2016-03-22 11:56:03 +0100

    fixed merge error

--- app/helper/hms/HmsUtil.scala
+++ app/helper/hms/HmsUtil.scala
@@ -1,13 +1,7 @@
 package helper.hms
 
-import java.util.concurrent.TimeUnit
-
 import helper.Config
 import models.Station
-
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 /**
   * author: cvandrei
@@ -24,6 +18,7 @@ object HmsUtil {
       case Some(station) =>
 
         val baseUrl = Config.hmsBroadcastUrl
+        val channelId = station.channelId
         val encStationID = java.net.URLEncoder.encode(station.stationId, "UTF-8")
         val encHmsStationID = java.net.URLEncoder.encode(station.hmsStationId, "UTF-8")
 
