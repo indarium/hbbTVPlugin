3bdf52f06bafe220215b3b5b7e30294c608631a8
admin user <admin@static.19.138.99.88.clients.your-server.de>
2019-02-03 15:06:24 +0000

    fixed statup script

--- start_with_secrets.sh
+++ start_with_secrets.sh
@@ -11,7 +11,7 @@ USER_HOME=/opt/playApps/hbbTVPlugin
 # export PATH=$JAVA_HOME/bin:$USER_HOME/play_home:$PATH
 
 # Path to the application
-APP_PATH=./target/universal/stage
+APP_PATH=./target/resolution-cache
 APP_OPTS="-Dconfig.file=../hbbTVPluginSecrets/application_prod.conf"
 
 RETVAL=0
@@ -21,7 +21,7 @@ case "$1" in
     echo -n "Starting Play service"
     rm -f ${APP_PATH}/RUNNING_PID
     #su $USER -c "$APP_PATH/bin/hbbtvplugin $APP_OPTS >/dev/null" &
-    $APP_PATH/bin/hbbtvplugin $APP_OPTS >/dev/null &
+    $APP_PATH/hbbtvplugin $APP_OPTS >/dev/null &
     RETVAL=$?
     ;;
   stop)
